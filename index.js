8ffe028f9215fc607dd3d6d12166430029e536de
AbtahiHasan <anikidpersonal@gmail.com>
2023-05-10 11:42:55 +0600

    crud oparation done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.oasvurr.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -23,7 +23,50 @@ async function run() {
   try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");\
+
+    const database = client.db("chocolate-circle")
+    const chocolatesCollection = database.collection("chocolates")
+
+    app.get("/", async(req, res) => {
+        const chocolates = chocolatesCollection.find()
+        const result = await chocolates.toArray()
+        res.send(result)
+    })
+    app.get("/:id", async(req, res) => {
+        const id = req.params.id 
+        const chocolate = await chocolatesCollection.findOne({_id: new ObjectId(id)})
+        res.send(chocolate)
+    })
+
+    app.post("/", async (req, res) => {
+        const chocolate = req.body
+        const result = await chocolatesCollection.insertOne(chocolate)
+        res.send(result)
+    })
+
+    app.put("/:id", async (req, res) => {
+        const id = req.params.id
+        const chocolate = req.body
+        const result = await chocolatesCollection.updateOne({_id: new ObjectId(id)}, {
+            $set: {
+                name: chocolate.name,
+                country: chocolate.country,
+                category: chocolate.category
+            }},
+            {
+                upsert: true
+            }
+        )
+        res.send(result)
+    })
+
+    app.delete("/:id", (req, res) => {
+        const id = req.params.id
+        const result = chocolatesCollection.deleteOne({_id: new ObjectId(id)})
+        res.send(result)
+    })
+
   } catch(error) {
     console.log(error)
   }
